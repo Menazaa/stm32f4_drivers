66197d2c54bb4a035fade0a82235326c67abd030
Mohamed Ahmed <myyli1999@gmail.com>
2023-07-29 14:59:13 +0300

    add LIB layer files

---
+++ LIB/STD_TYPES.h
@@ -0,0 +1,24 @@
+/*
+ * STD_TYPES.h
+ *
+ *  Created on: Aug 3, 2022
+ *      Author: Mohamed Menazaa
+ */
+
+#ifndef STD_TYPES_H_
+#define STD_TYPES_H_
+
+
+// u: Unsigned - s: Signed - f: Float
+// 8,16,32: Number of bits
+
+typedef		unsigned char		u8		;
+typedef		unsigned short int	u16		;
+typedef		unsigned int		u32		;
+
+typedef		signed char			s8		;
+typedef		signed short int	s16		;
+typedef		signed int			s32		;
+
+
+#endif /* STD_TYPES_H_ */
